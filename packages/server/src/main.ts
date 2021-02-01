2380a6c3009d5dded97463e16c877fc027efd429
Alyx Mote <figments@outlook.com>
2021-02-01 09:52:40 -0800

    added proper tabs

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -13,7 +13,7 @@ import { AppModule } from './app/app.module';
  */
 const results: DotenvConfigOutput = config();
 if (results.error) {
-  console.log(`You don't have a .env file set up! Are you sure the environment variables are configured?`);
+    console.log(`You don't have a .env file set up! Are you sure the environment variables are configured?`);
 }
 
 /**
@@ -34,22 +34,22 @@ async function determineEnv(): Promise<string> {
  * Bootstraps the web server.
  */
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
-  app.use(json({limit: '50mb'}));
-  app.use(urlencoded({limit: '50mb', extended: true}));
-  app.use(csurf({cookie: true}));
-  app.use(function (req, res, next) {
-    res.cookie('XSRF-TOKEN', req.csrfToken()); // this is to make sure the XSRF-TOKEN is being set correctly
-    return next();
-  });
-  app.use(helmet());
-  app.enableCors({origin: await determineEnv(), credentials: true});
-  app.use(require('prerender-node').set('prerenderToken', process.env.PRERENDER_TOKEN));
-  const port = process.env.PORT || 3333;
-  await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/');
-  });
+    const app = await NestFactory.create(AppModule);
+    app.use(cookieParser());
+    app.use(json({limit: '50mb'}));
+    app.use(urlencoded({limit: '50mb', extended: true}));
+    app.use(csurf({cookie: true}));
+    app.use(function (req, res, next) {
+        res.cookie('XSRF-TOKEN', req.csrfToken()); // this is to make sure the XSRF-TOKEN is being set correctly
+        return next();
+    });
+    app.use(helmet());
+    app.enableCors({origin: await determineEnv(), credentials: true});
+    app.use(require('prerender-node').set('prerenderToken', process.env.PRERENDER_TOKEN));
+    const port = process.env.PORT || 3333;
+    await app.listen(port, () => {
+        Logger.log('Listening at http://localhost:' + port + '/');
+    });
 }
 
 bootstrap();
